04cb016a9e1c8d8f82f551e3b5527fe2c5a3763b
LaliteshMuddapati <lallivedu@gmail.com>
2026-03-01 16:19:36 +0530

    Add files via upload

---
+++ lines in file.py
@@ -0,0 +1,14 @@
+file=open('Codingal.txt', 'r')
+
+counter=0
+
+content=file.read()
+
+colist=content.split("\n")
+
+for i in colist:
+    if i:
+        counter+=1
+
+print("This is the number of lines in the file")
+print(counter)
